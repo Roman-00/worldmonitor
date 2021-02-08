bb863fcc4f155fd564cc243ad0cad98c43b70ba5
Roman-00 <rvasin00@gmail.com>
2021-02-08 11:42:05 +0600

    feat: add a gulp projects

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const {src, dest, watch, series} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+
+// Static server
+function bs() {
+  serveSass();
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./sass/*.sass", "./sass/**/*.sass", "./sass/**/*.scss")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+function buildCss(done) {
+  src('./css/**/*.css')
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(dest('./dist/css/'));
+  done();
+};
+
+function buildJs(done) {
+  src(['./js/**/*.js', '!./js/**/*.min.js'])
+  .pipe(minify({ext:{
+      min:'.js'
+    }
+  }))
+  .pipe(dest('./dist/js/'));
+  src('./js/**/*.min.js').pipe(dest('./dist/js/'));
+  done();
+};
+
+function html(done) {
+  src('./**/*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(dest('./dist/'));
+  done();
+};
+
+function fonts(done) {
+  src('./font/**/**')
+  .pipe(dest('./dist/fonts/'));
+  done();
+};
+
+
+exports.serve = bs;
+exports.build = series(buildCss, buildJs, html, fonts);
